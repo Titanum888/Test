e04eabfc0f5ea016a04e688d20a24c73971205e3
Ksuha <dklfjdfijdpkof@gmail.com>
2019-08-31 11:08:09 +0300

    best commit

--- Info.cs
+++ Info.cs
@@ -5,5 +5,11 @@ class Program
     static void Main(string [] args)
     {
         System.Console.WriteLine("Hello world");    
+        MyBestMethod();
+    }
+
+    static void MyBestMethod()
+    {
+        Console.WriteLine("Тут была Ксюха!");
     }
 }
